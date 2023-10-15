c0697e0082ddefd2401bd4d6a4fabd28c2023d01
Saiffer <aznaurovdato@gmail.com>
2023-10-15 17:43:52 -0400

    Added Typescript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,23 @@
 import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
 import Footer from "@/components/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Blog App",
   description: "The best blog app!",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
+    <html>
       <body className={inter.className}>
         <div className="container">
           <div className="wrapper">
@@ -26,3 +31,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
