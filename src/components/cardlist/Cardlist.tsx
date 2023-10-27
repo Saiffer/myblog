bceb6cc226d1786108dcf509a68640d4788f0bd3
Saiffer <aznaurovdato@gmail.com>
2023-10-26 20:07:07 -0400

    Added Carlist section

--- src/components/cardlist/Cardlist.tsx
+++ src/components/cardlist/Cardlist.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import styles from './cardlist.module.css';
 import Pagination from '../pagination/Pagination';
+import Card from '../card/Card';
 
 const Cardlist = () => {
   return (
-    <div className={styles.container}>Cardlist
+    <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <Card />
+        </div>
+      </div>
     <Pagination />
     </div>
     
