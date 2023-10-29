f360f06b662211087ee2042c1177d5b1821b8026
Saiffer <aznaurovdato@gmail.com>
2023-10-29 19:01:12 -0400

    refactoring

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,42 +1,46 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react';
-import styles from './write.module.css';
-import Image from 'next/image';
-import ReactQuill from 'react-quill';
+import React, { useState } from "react";
+import styles from "./write.module.css";
+import Image from "next/image";
+import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
 
 const Write = () => {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
 
- 
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Title" className={styles.input}/>
+      <input type="text" placeholder="Title" className={styles.input} />
       <div className={styles.editor}>
-        <button className={styles.button}  onClick={()=>setOpen(!open)}>
-          <Image src="/plus.png" alt="" width={16} height={16}/>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
+          <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
-        {
-          open && (
-            <div className={styles.add}>
-              <button className={styles.addButton}>
-          <Image src="/image.png" alt="" width={16} height={16}/>
-        </button>
-        <button className={styles.addButton}>
-          <Image src="/external.png" alt="" width={16} height={16}/>
-        </button>
-        <button className={styles.addButton}>
-          <Image src="/video.png" alt="" width={16} height={16}/>
-        </button>
-              </div>
-          )}
-          <ReactQuill theme="bubble" value={value} onChange={setValue} placeholder="Tell your story..." className={styles.textArea}/>
+        {open && (
+          <div className={styles.add}>
+            <button className={styles.addButton}>
+              <Image src="/image.png" alt="" width={16} height={16} />
+            </button>
+            <button className={styles.addButton}>
+              <Image src="/external.png" alt="" width={16} height={16} />
+            </button>
+            <button className={styles.addButton}>
+              <Image src="/video.png" alt="" width={16} height={16} />
+            </button>
+          </div>
+        )}
+        <ReactQuill
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your story..."
+          className={styles.textArea}
+        />
       </div>
       <button className={styles.publish}>Publish</button>
     </div>
-  )
-}
+  );
+};
 
-export default Write
+export default Write;
